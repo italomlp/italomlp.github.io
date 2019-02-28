2226811a1d8c7cf0f30fe0f00e48cf95538277d1
Italo Menezes <italomlporoca@hotmail.com>
2019-02-28 13:41:11 -0300

    make site with Gatsby

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import { withPrefix } from 'gatsby'
-import siteConfig from '../../../data/siteConfig'
-import styled from 'styled-components'
+import React from 'react';
+import { withPrefix } from 'gatsby';
+import siteConfig from '../../../data/siteConfig';
+import styled from 'styled-components';
 
 const HeroContainer = styled.div`
   position: relative;
@@ -11,14 +11,14 @@ const HeroContainer = styled.div`
   background-repeat: no-repeat;
   background-position: center;
   background-size: cover;
-`
+`;
 
 const TitleContainer = styled.div`
   display: table-cell;
   vertical-align: middle;
   text-align: center;
   width: 100%;
-`
+`;
 
 const HeroTitle = styled.h1`
   font-weight: 700;
@@ -26,11 +26,11 @@ const HeroTitle = styled.h1`
   margin: 10px 60px;
   color: #fff;
   text-shadow: 1px 1px 4px rgba(34, 34, 34, 0.6);
-`
+`;
 
 class Hero extends React.Component {
   render() {
-    const { title, className } = this.props
+    const { title, className } = this.props;
 
     return (
       <HeroContainer className={className}>
@@ -38,16 +38,16 @@ class Hero extends React.Component {
           <HeroTitle>{title}</HeroTitle>
         </TitleContainer>
       </HeroContainer>
-    )
+    );
   }
 }
 
 export default styled(Hero)`
-  
-  ${p => `background-image: url(${p.heroImg || withPrefix(siteConfig.siteCover)});`}
+  ${p =>
+    `background-image: url(${p.heroImg || withPrefix(siteConfig.siteCover)});`}
   height: 70vh;
   background-attachment: fixed;
   background-position: center;
   background-repeat: no-repeat;
   background-size: cover;
-`
+`;
--- src/components/timeline/timeline.js
+++ src/components/timeline/timeline.js
@@ -1,30 +1,38 @@
-import React from 'react'
-import styled from 'styled-components'
-import { config } from 'react-awesome-styled-grid'
-import siteConfig from '../../../data/siteConfig'
+import React from 'react';
+import styled from 'styled-components';
+import { config } from 'react-awesome-styled-grid';
+import siteConfig from '../../../data/siteConfig';
 
 const Timeline = ({ className }) => {
   return (
     <div className={className}>
-      <h1>Experience</h1>
-      {siteConfig.jobs && siteConfig.jobs.map(job => (
-        <article key={job.begin.month + job.begin.year} className='timeline__item'>
-          <div className="inner">
-            <span className="timeline__date">
-              <span className="timeline__month">{job.begin.month}</span>
-              <span className="timeline__year">{job.begin.year}</span>
-            </span>
-            <h2 className='timeline__title'>{job.occupation} at {job.company} <br /><small className='timeline__title--small'>({job.duration || 'present'})</small></h2>
-            <p>{job.description}</p>
-          </div>
-        </article>
-      ))}
+      <h1>Experiência</h1>
+      {siteConfig.jobs &&
+        siteConfig.jobs.map(job => (
+          <article
+            key={job.begin.month + job.begin.year}
+            className="timeline__item"
+          >
+            <div className="inner">
+              <span className="timeline__date">
+                <span className="timeline__month">{job.begin.month}</span>
+                <span className="timeline__year">{job.begin.year}</span>
+              </span>
+              <h2 className="timeline__title">
+                {job.occupation} at {job.company} <br />
+                <small className="timeline__title--small">
+                  ({job.duration || 'atual'})
+                </small>
+              </h2>
+              <p>{job.description}</p>
+            </div>
+          </article>
+        ))}
     </div>
-  )
-}
+  );
+};
 
 export default styled(Timeline)`
-
   position: relative;
   :before {
     content: '';
@@ -35,7 +43,7 @@ export default styled(Timeline)`
     margin: 70px 0 0 -1px;
     width: 1px;
     height: calc(100% - 70px);
-    background: #25303B;
+    background: #25303b;
   }
   .timeline__item {
     width: 100%;
@@ -52,7 +60,7 @@ export default styled(Timeline)`
     float: left;
     margin: 85px 0 0 0;
     border-radius: 6px;
-    border: 1px solid #25303B;
+    border: 1px solid #25303b;
   }
   .timeline__date {
     display: block;
@@ -66,7 +74,7 @@ export default styled(Timeline)`
     font-size: 12px;
     font-weight: 900;
     text-transform: uppercase;
-    background: #25303B;
+    background: #25303b;
     color: #fff;
     box-shadow: 0 0 0 7px #fff;
   }
@@ -95,7 +103,7 @@ export default styled(Timeline)`
     position: absolute;
     top: -5px;
     left: 30%;
-    width: 10px; 
+    width: 10px;
     height: 10px;
     transform: rotate(-45deg);
   }
@@ -107,14 +115,14 @@ export default styled(Timeline)`
     color: #656565;
     border-radius: 0 0 6px 6px;
   }
-  .timeline__item:nth-child(2n+2) div.inner {
+  .timeline__item:nth-child(2n + 2) div.inner {
     float: right;
   }
   .timeline__title {
-    background: #25303B;
+    background: #25303b;
   }
   .timeline__title:after {
-    background: #25303B;
+    background: #25303b;
   }
 
   .timeline__title--small {
@@ -137,4 +145,4 @@ export default styled(Timeline)`
   }
 
   `}
-`
+`;
