2910432ce12d654f1490af9c6d19e3fb45e60633
Italo Menezes <italomlporoca@hotmail.com>
2019-03-06 21:14:29 -0300

    add google adsense code

--- src/components/SEO/seo.js
+++ src/components/SEO/seo.js
@@ -60,7 +60,17 @@ function SEO({ description, lang, meta, keywords, title }) {
                   : []
               )
               .concat(meta)}
-          />
+          >
+            <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+            <script>
+              {`
+                (adsbygoogle = window.adsbygoogle || []).push({
+                  google_ad_client: "ca-pub-9886266841154637",
+                  enable_page_level_ads: true
+                });
+              `}
+            </script>
+          </Helmet>
         )
       }}
     />
