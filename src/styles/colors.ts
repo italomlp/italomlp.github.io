cb1269458f048abe6a1a5e23406fd9de2078c341
Italo Menezes <italomlporoca@hotmail.com>
2020-04-20 15:54:51 -0300

    Change basics colors

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,27 +1,15 @@
-import Color from 'color';
+import { darken, lighten } from 'polished';
 
 const colors = {
   primary: '#425887',
-  primaryLight: Color('#425887')
-    .lighten(0.2)
-    .toString(),
-  primaryDark: Color('#425887')
-    .darken(0.2)
-    .toString(),
-  secondary: '#da0000',
-  secondaryDark: Color('#da0000')
-    .darken(0.2)
-    .toString(),
-  secondaryLight: Color('#da0000')
-    .lighten(0.2)
-    .toString(),
-  other: '#dddcf2',
-  otherDark: Color('#dddcf2')
-    .darken(0.2)
-    .toString(),
-  otherLight: Color('#dddcf2')
-    .lighten(0.2)
-    .toString(),
+  primaryLight: lighten(0.2)('#425887'),
+  primaryDark: darken(0.2)('#425887'),
+  secondary: '#2B8682',
+  secondaryDark: darken(0.2)('#2B8682'),
+  secondaryLight: lighten(0.2)('#2B8682'),
+  other: '#D3EAE8',
+  otherDark: darken(0.2)('#D3EAE8'),
+  otherLight: lighten(0.2)('#D3EAE8'),
 
   dark: '#000',
   light: '#fff',
