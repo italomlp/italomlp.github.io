719d069eecf4d0a2fea1f25152dec0bb43f30a7a
Italo Menezes <italomlp@gmail.com>
2023-10-18 18:25:18 -0300

    chore: make locale switcher component server side

--- src/components/header/locale-switcher.tsx
+++ src/components/header/locale-switcher.tsx
@@ -1,57 +1,9 @@
-'use client';
-import { twMerge } from 'tailwind-merge';
-
-import {
-  Button,
-  Tooltip,
-  TooltipContent,
-  TooltipTrigger,
-} from '@/components/ui';
-import { useChangeLocale } from '@/locales/client';
+import { LanguageButton } from '@/components/header/language-button';
 
 type Props = {
   currentLocale: string;
   changeLanguageLabel: string;
 };
-
-type LanguageButtonProps = {
-  language: string;
-  currentLocale: string;
-  changeLanguageLabel: string;
-} & Omit<React.ComponentProps<typeof Button>, 'onClick' | 'children'>;
-
-function LanguageButton({
-  className,
-  language,
-  currentLocale,
-  changeLanguageLabel,
-  ...props
-}: LanguageButtonProps) {
-  const changeLocale = useChangeLocale();
-
-  if (currentLocale === language) {
-    return null;
-  }
-
-  return (
-    <Tooltip delayDuration={300}>
-      <TooltipTrigger asChild>
-        <Button
-          variant="ghost"
-          size="icon"
-          className={twMerge('self-center opacity-90', className)}
-          onClick={() => {
-            changeLocale(language as 'en' | 'pt');
-          }}
-          {...props}
-        >
-          {language.toUpperCase()}
-        </Button>
-      </TooltipTrigger>
-      <TooltipContent side="bottom">{changeLanguageLabel}</TooltipContent>
-    </Tooltip>
-  );
-}
 
 export function LocaleSwitcher({ currentLocale, changeLanguageLabel }: Props) {
   return (
